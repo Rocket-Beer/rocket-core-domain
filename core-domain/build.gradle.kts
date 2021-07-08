9951c6c2d9901f1a3fd25d41af97d93bac8e4421
Rui Miguel Alonso <ruiskas@gmail.com>
2021-07-08 20:38:52 +0200

    Build: applied gradle config changes

--- core-domain/build.gradle.kts
+++ core-domain/build.gradle.kts
@@ -1,8 +1,3 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import java.io.FileInputStream
-import java.io.InputStreamReader
-import java.util.Properties
-
 plugins {
     id("kotlin")
     id("java-library")
@@ -22,54 +17,4 @@ dependencies {
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.20")
     testImplementation("io.mockk:mockk:1.12.0")
-}
-
-publishing {
-    repositories {
-        maven {
-            name = getPublishProperty("github.name")
-            url = uri(getPublishProperty("github.url"))
-            credentials {
-                username = gradleLocalProperties(rootDir).getProperty("github.username")
-                password = gradleLocalProperties(rootDir).getProperty("github.password")
-            }
-        }
-    }
-
-    publications {
-        create<MavenPublication>("gpr") {
-            run {
-                groupId = getPublishProperty("groupId")
-                artifactId = getPublishProperty("artifactId")
-                version = getPublishProperty("version")
-                artifact("$buildDir/libs/$artifactId.jar")
-            }
-        }
-    }
-}
-
-fun gradlePublisherProperties(projectRootDir: File): Properties {
-    val properties = Properties()
-    val publisherProperties = File(projectRootDir, "publisher.properties")
-
-    if (publisherProperties.isFile) {
-        InputStreamReader(FileInputStream(publisherProperties), Charsets.UTF_8).use { reader ->
-            properties.load(reader)
-        }
-    }
-    return properties
-}
-
-fun getPublishProperty(property: String): String {
-    return gradlePublisherProperties(projectDir).getProperty(property) ?: ""
-}
-
-fun getPublisherUsername(): String {
-    return gradleLocalProperties(rootDir).getProperty("github.username")
-        ?: System.getenv("GITHUB_ACTOR")
-}
-
-fun getPublisherPassword(): String {
-    return gradleLocalProperties(rootDir).getProperty("github.password")
-        ?: System.getenv("GITHUB_TOKEN")
 }
