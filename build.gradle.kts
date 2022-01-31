d58a6f34f7ce6e11b95a89bf8ec8295585514dd3
Guillermo <>
2022-01-31 13:25:20 +0100

    feature: settings migrated from groovy to kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,3 @@
-/*buildscript {
-    repositories {
-        maven { url = "https://plugins.gradle.org/m2/" }
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:4.2.1")
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20"
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
-        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.17.1")
-    }
-}*/
-
 allprojects {
     repositories {
         google()
